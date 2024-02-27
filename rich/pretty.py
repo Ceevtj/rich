e7e11b00eb0ad9ac98d66594fba2172944ca9a3d
TerenceNg03 <terenceng03@outlook.com>
2024-02-28 00:43:20 +0100

    Fix format

--- rich/pretty.py
+++ rich/pretty.py
@@ -378,12 +378,15 @@ _MAPPING_CONTAINERS = (dict, os._Environ, MappingProxyType, UserDict)
 
 def is_expandable(obj: Any) -> bool:
     """Check if an object may be expanded by pretty print."""
-    return ((
-        _safe_isinstance(obj, _CONTAINERS)
-        or (is_dataclass(obj))
-        or (hasattr(obj, "__rich_repr__"))
-        or _is_attr_object(obj)
-    ) and not isclass(obj)) or (isclass(obj) and hasattr(obj, '__rich_class_repr__'))
+    return (
+        (
+            _safe_isinstance(obj, _CONTAINERS)
+            or (is_dataclass(obj))
+            or (hasattr(obj, "__rich_repr__"))
+            or _is_attr_object(obj)
+        )
+        and not isclass(obj)
+    ) or (isclass(obj) and hasattr(obj, "__rich_class_repr__"))
 
 
 @dataclass
